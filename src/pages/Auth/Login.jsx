5d0e4cbe4d7b35c1d2dd0696ac7294d5e60ae1b5
JustTrying652 <ngataevans@gmail.com>
2026-07-01 21:12:29 +0300

    bad access

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -56,7 +56,7 @@ export default function Login() {
         <form onSubmit={handleLogin} className="space-y-5">
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
-              Email Address
+              Email 
             </label>
             <input
               type="email"
