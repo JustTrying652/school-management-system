39f77bd0ca461299e84b543116acd6a6fcd13c5c
JustTrying652 <ngataevans@gmail.com>
2026-07-03 21:22:00 +0300

    portal landing

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -454,7 +454,7 @@ export default function Classes() {
                   onChange={(e) => setTransferClassId(e.target.value)}
                   className="w-full border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                 >
-                  <option value="">Select new class</option>
+                  <option value="">Select new </option>
                   {classes
                     .filter((c) => c.id !== selectedClass?.id)
                     .map((c) => (
